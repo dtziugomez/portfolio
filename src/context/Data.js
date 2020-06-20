41aef5d00c1d5473588bcc84e998258d0422c6aa
dtziugomez <dtziugomez@gmail.com>
2020-06-20 00:05:38 -0500

    clear file Data

--- src/context/Data.js
+++ src/context/Data.js
@@ -8,92 +8,11 @@ export const Links = [
     scrolling: "Home"
   },
 
-  
-
-  {
-    id: 3,
-    link: "Skills",
-    path: "/skills",
-    scrolling: "Skills"
-  },
-
   {
     id: 4,
     link: "Projects",
     path: "/projects",
     scrolling: "Projects"
-  }
-
-  
-  
-];
-
-// skills development
-// options for percent:ninety,eighty,senenty,eighty,fifty,thirty
-export const devSkills = [
-  {
-    id: 1,
-    title: "HTML5",
-    percent: "ninety",
-    number: "90%"
-  },
-  {
-    id: 2,
-    title: "CSS3",
-    percent: "ninety",
-    number: "90%"
-  },
-  {
-    id: 3,
-    title: "JAVASCRIPT",
-    percent: "eighty",
-    number: "80%"
-  },
-  {
-    id: 4,
-    title: "REACT",
-    percent: "eighty",
-    number: "80%"
-  },
-  {
-    id: 5,
-    title: "BOOTSTRAP",
-    percent: "fifty",
-    number: "50%"
-  },
-  {
-    id: 6,
-    title: "NODE JS",
-    percent: "thirty",
-    number: "30%"
-  }
-];
-
-// skills electrical
-export const electricSkills = [
-  {
-    id: 1,
-    title: "ELECTRICAL STUDIES",
-    percent: "ninety",
-    number: "90%"
-  },
-  {
-    id: 2,
-    title: "DOMESTIC INSTALLATIONS",
-    percent: "ninety",
-    number: "90%"
-  },
-  {
-    id: 3,
-    title: "PHOTOVOLTAIC SYSTEMS",
-    percent: "ninety",
-    number: "90%"
-  },
-  {
-    id: 4,
-    title: "ELECTRICAL AUTOMOTIONS",
-    percent: "eighty",
-    number: "80%"
   }
 ];
 
@@ -140,39 +59,5 @@ export const projects = [
     img: "/photos/project1.jpg",
     title: "react",
     btnTitle: "project"
-  }
-];
-
-// courses
-export const courses = [
-  {
-    id: 1,
-    iframe: "https://www.youtube.com/embed/iudR_4MBRyU",
-    title: "Lti website"
-  },
-  {
-    id: 2,
-    iframe: "https://www.youtube.com/embed/vJ0AVaO31xE",
-    title: "Autosafe website"
-  },
-  {
-    id: 3,
-    iframe: "https://www.youtube.com/embed/o3eR0X91Ogs",
-    title: "React contact form"
-  },
-  {
-    id: 4,
-    iframe: "https://www.youtube.com/embed/iudR_4MBRyU",
-    title: "Lti website"
-  },
-  {
-    id: 5,
-    iframe: "https://www.youtube.com/embed/iudR_4MBRyU",
-    title: "Lti website"
-  },
-  {
-    id: 6,
-    iframe: "https://www.youtube.com/embed/iudR_4MBRyU",
-    title: "Lti website"
   }
 ];
