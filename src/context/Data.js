51a006916f6e19906a89aefd05b804b382220638
dtziugomez <dtziugomez@gmail.com>
2020-07-21 11:22:16 -0500

    update projects

--- src/context/Data.js
+++ src/context/Data.js
@@ -9,25 +9,32 @@ export const Links = [
 
 // projects
 export const projects = [
-  {
-    id: 1,
-    extlink: "https://cashmoney.offixsiap.mx",
-    img: "/photos/cashmoney.png",
-    title: "CashMoney",
-    btnTitle: "Visitar"
-  },
-  {
-    id: 2,
-    extlink: "http://ventasdi.no-ip.net/erpdin",
-    img: "/photos/erpdin.PNG",
-    title: "Erpdin",
-    btnTitle: "Visitar"
-  },
-  {
-    id: 3,
-    extlink: "#",
-    img: "/photos/preguntas-app.PNG",
-    title: "EncuestaDin",
-    btnTitle: ""
-  }
-];
+         {
+           id: 1,
+           extlink: "https://cashmoney.offixsiap.mx",
+           img: "/photos/cashmoney.png",
+           title: "CashMoney",
+           btnTitle: "Visitar"
+         },
+         {
+           id: 2,
+           extlink: "http://ventasdi.no-ip.net/erpdin",
+           img: "/photos/erpdin.PNG",
+           title: "Erpdin",
+           btnTitle: "Visitar"
+         },
+         {
+           id: 3,
+           extlink: "#",
+           img: "/photos/preguntas-app.PNG",
+           title: "EncuestaDin",
+           btnTitle: ""
+         },
+         {
+           id: 4,
+           extlink: "https://heuristic-blackwell-53a086.netlify.app/",
+           img: "/photos/covid-yucatan-mexico.PNG",
+           title: "COVID 19 IN YUCATAN-MEXICO",
+           btnTitle: "Visitar"
+         }
+       ];
