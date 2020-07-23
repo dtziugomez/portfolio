b62d49af878d700e4ad779291307261c4cfb80d1
dtziugomez <dtziugomez@gmail.com>
2020-07-22 21:05:39 -0500

    updtate buton visitar

--- src/context/Data.js
+++ src/context/Data.js
@@ -9,57 +9,58 @@ export const Links = [
 
 // projects
 export const projects = [
-  {
-    id: 1,
-    extlink: "https://cashmoney.offixsiap.mx",
-    img: "/photos/cashmoney.png",
-    title: "CashMoney",
-    btnTitle: "Visitar",
-    active: true,
-    details: {
-      title: "",
-      description: "",
-      images: []
-    }
-  },
-  {
-    id: 2,
-    extlink: "http://ventasdi.no-ip.net/erpdin",
-    img: "/photos/erpdin.PNG",
-    title: "Erpdin",
-    btnTitle: "Visitar",
-    active: false,
-    details: {
-      title: "Erpdin",
-      description:
-        "ERP with modules: Resources humans, Reports, and crontrol of documents.",
-      images: ["/photos/erpdin.PNG"]
-    }
-  },
-  {
-    id: 3,
-    extlink: "",
-    img: "/photos/encuestadin/preguntas-app.PNG",
-    title: "EncuestaDin",
-    btnTitle: "Visitar",
-    active: false,
-    details: {
-      title: "Encuestadin",
-      description: "An app to realize surveys developed with android studio.",
-      images: ["/photos/encuestadin/4.jpg"]
-    }
-  },
-  {
-    id: 4,
-    extlink: "https://heuristic-blackwell-53a086.netlify.app/",
-    img: "/photos/covid-yucatan-mexico.PNG",
-    title: "COVID 19 IN YUCATAN-MEXICO",
-    btnTitle: "Visitar",
-    active: true,
-    details: {
-      title: "",
-      description: "",
-      images: []
-    }
-  }
-];
+         {
+           id: 1,
+           extlink: "https://cashmoney.offixsiap.mx",
+           img: "/photos/cashmoney.png",
+           title: "CashMoney",
+           btnTitle: "Visitar",
+           active: true,
+           details: {
+             title: "",
+             description: "",
+             images: []
+           }
+         },
+         {
+           id: 2,
+           extlink: "/details/2",
+           img: "/photos/erpdin.PNG",
+           title: "Erpdin",
+           btnTitle: "Visitar",
+           active: false,
+           details: {
+             title: "Erpdin",
+             description:
+               "ERP with modules: Resources humans, Reports, and crontrol of documents.",
+             images: ["/photos/erpdin.PNG"]
+           }
+         },
+         {
+           id: 3,
+           extlink: "/details/3",
+           img: "/photos/encuestadin/preguntas-app.PNG",
+           title: "EncuestaDin",
+           btnTitle: "Visitar",
+           active: false,
+           details: {
+             title: "Encuestadin",
+             description:
+               "An app to realize surveys developed with android studio.",
+             images: ["/photos/encuestadin/4.jpg"]
+           }
+         },
+         {
+           id: 4,
+           extlink: "https://heuristic-blackwell-53a086.netlify.app/",
+           img: "/photos/covid-yucatan-mexico.PNG",
+           title: "COVID 19 IN YUCATAN-MEXICO",
+           btnTitle: "Visitar",
+           active: true,
+           details: {
+             title: "",
+             description: "",
+             images: []
+           }
+         }
+       ];
