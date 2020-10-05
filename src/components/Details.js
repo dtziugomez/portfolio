a02ae6f87bab984e6f2a563d927e06738e38953a
dtziugomez <dtziugomez@gmail.com>
2020-10-05 09:02:37 -0500

    add effect fade

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,26 +2,29 @@ import React from "react";
 import { projects } from "../context/Data";
 import Title from "../components/Title";
 import "../App.css";
+import Fade from "react-reveal/Fade";
 
 function Details(props) {
   let project = projects.filter(p => p.id === Number(props.match.params.id));
 
   return (
-    <div className="projects">
-      <div className="container">
-        <Title title={project[0].details.title} color="gray" />
-        <article>
-          <p>{project[0].details.description}</p>
-        </article>
-        <div className="projects-center">
-          {project[0].details.images.map(image => (
-            <div id="images" className="single-project-detail">
-              <img key={props.match.params.id++} src={image} alt="pic" />
-            </div>
-          ))}
+    <Fade left cascade>
+      <div className="projects">
+        <div className="container">
+          <Title title={project[0].details.title} color="gray" />
+          <article>
+            <p>{project[0].details.description}</p>
+          </article>
+          <div className="projects-center">
+            {project[0].details.images.map(image => (
+              <div id="images" className="single-project-detail">
+                <img key={props.match.params.id++} src={image} alt="pic" />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 }
 
