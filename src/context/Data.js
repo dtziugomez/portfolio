7111c33190b5ce004d875cac696ae32ac04c6879
dtziugomez <dtziugomez@gmail.com>
2020-10-08 11:11:16 -0500

    titles

--- src/context/Data.js
+++ src/context/Data.js
@@ -9,95 +9,95 @@ export const Links = [
 
 // projects
 export const projects = [
-         {
-           id: 1,
-           extlink: "https://cashmoney.offixsiap.mx",
-           img: "/photos/cashmoney.png",
-           title: "CashMoney",
-           btnTitle: "Visitar",
-           active: true,
-           details: {
-             title: "",
-             description: "",
-             images: []
-           }
-         },
-         {
-           id: 2,
-           extlink: "/details/2",
-           img: "/photos/erpdin.PNG",
-           title: "Erpdin",
-           btnTitle: "Visitar",
-           active: false,
-           details: {
-             title: "Erpdin",
-             description:
-               "ERP with modules: Resources humans, Reports, and control of documents.",
-             images: ["/photos/erpdin.PNG"]
-           }
-         },
-         {
-           id: 3,
-           extlink: "/details/3",
-           img: "/photos/encuestadin/bienvenida-app.PNG",
-           title: "EncuestaDin",
-           btnTitle: "Visitar",
-           active: false,
-           details: {
-             title: "Encuestadin",
-             description:
-               "An app to realize surveys developed with android studio.",
-             images: [
-               "/photos/encuestadin/preguntas-app.PNG",
-               "/photos/encuestadin/4.jpg"
-             ]
-           }
-         },
-         {
-           id: 4,
-           extlink: "https://heuristic-blackwell-53a086.netlify.app/",
-           img: "/photos/covid-yucatan-mexico.PNG",
-           title: "COVID 19 IN YUCATAN-MEXICO",
-           btnTitle: "Visitar",
-           active: true,
-           details: {
-             title: "",
-             description: "",
-             images: []
-           }
-         },
-         {
-           id: 5,
-           extlink: "/details/5",
-           img: "/photos/demo/demo.PNG",
-           title: "REGISTRO CIVIL",
-           btnTitle: "Visitar",
-           active: true,
-           details: {
-             title: "Registro Civil",
-             description: "",
-             images: [
-               "/photos/demo/demo.PNG",
-               "/photos/demo/demo2.PNG",
-               "/photos/demo/demo3.PNG"
-             ]
-           }
-         },
-         {
-           id: 6,
-           extlink: "/details/6",
-           img: "/photos/gyca/gyca1.PNG",
-           title: "Control de licencias",
-           btnTitle: "Visitar",
-           active: true,
-           details: {
-             title: "Control de licencias",
-             description: "",
-             images: [
-               "/photos/gyca/gyca1.PNG",
-               "/photos/gyca/gyca2.PNG",
-               "/photos/gyca/gyca3.PNG"
-             ]
-           }
-         }
-       ];
+  {
+    id: 1,
+    extlink: "https://cashmoney.offixsiap.mx",
+    img: "/photos/cashmoney.png",
+    title: "CashMoney",
+    btnTitle: "Visitar",
+    active: true,
+    details: {
+      title: "",
+      description: "A website to register loans and customer payments.",
+      images: []
+    }
+  },
+  {
+    id: 2,
+    extlink: "/details/2",
+    img: "/photos/erpdin.PNG",
+    title: "Erpdin",
+    btnTitle: "Visitar",
+    active: false,
+    details: {
+      title: "Erpdin",
+      description:
+        "ERP with modules: Resources humans, Reports, and control of documents.",
+      images: ["/photos/erpdin.PNG"]
+    }
+  },
+  {
+    id: 3,
+    extlink: "/details/3",
+    img: "/photos/encuestadin/bienvenida-app.PNG",
+    title: "EncuestaDin",
+    btnTitle: "Visitar",
+    active: false,
+    details: {
+      title: "Encuestadin",
+      description: "An app to realize surveys.",
+      images: [
+        "/photos/encuestadin/preguntas-app.PNG",
+        "/photos/encuestadin/4.jpg"
+      ]
+    }
+  },
+  {
+    id: 4,
+    extlink: "https://heuristic-blackwell-53a086.netlify.app/",
+    img: "/photos/covid-yucatan-mexico.PNG",
+    title: "COVID 19 IN YUCATAN-MEXICO",
+    btnTitle: "Visitar",
+    active: true,
+    details: {
+      title: "",
+      description:
+        "Statistics about covid-19 in municipalities of yucatan state of mexico.",
+      images: []
+    }
+  },
+  {
+    id: 5,
+    extlink: "/details/5",
+    img: "/photos/demo/demo.PNG",
+    title: "Civil registration",
+    btnTitle: "Visitar",
+    active: true,
+    details: {
+      title: "Civil registration",
+      description: "Software desktop to control of users",
+      images: [
+        "/photos/demo/demo.PNG",
+        "/photos/demo/demo2.PNG",
+        "/photos/demo/demo3.PNG"
+      ]
+    }
+  },
+  {
+    id: 6,
+    extlink: "/details/6",
+    img: "/photos/gyca/gyca1.PNG",
+    title: "License control",
+    btnTitle: "Visitar",
+    active: true,
+    details: {
+      title: "License control",
+      description: "A website to control software versions.",
+      images: [
+        "/photos/gyca/gyca1.PNG",
+        "/photos/gyca/gyca2.PNG",
+        "/photos/gyca/gyca3.PNG"
+      ]
+    }
+  }
+];
